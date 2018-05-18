1f448869c3809fd03a73adba70c80e6e6594b779
tangbin <15882253352@163.com>
2018-05-18 15:41:17 +0800

    done

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 const http = require('http')
 const chalk = require('chalk')
+const path = require('path')
 const conf = require('./config/defaultConfig')
-console.log('aaaa')
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  res.end('<html><body>Hello World!</body></html>')
-})
+const route = require('./helper/route')
+const openUrl = require('./helper/openUrl')
 
-server.listen(conf.port, conf.hostname, () => {
-  const addr = `http://${conf.hostname}:${conf.port}`
-  console.info(`Server started at ${chalk.green(addr)}`)
-})
+class Server {
+  constructor (config) {
+    this.conf = Object.assign({}, conf, config)
+  }
+
+  start() {
+    const server = http.createServer((req, res) => {
+      const filePath = path.join(this.conf.root, req.url)
+      route(req, res, filePath, this.conf)
+    })
+    
+    server.listen(this.conf.port, this.conf.hostname, () => {
+      const addr = `http://${this.conf.hostname}:${this.conf.port}`
+      console.info(`Server started at ${chalk.green(addr)}`)
+      openUrl(addr)
+    })
+    
+  }
+}
+
+module.exports = Server
